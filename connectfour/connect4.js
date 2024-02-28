6ebb9a65ec858434724b1525af001ee4e4f5113c
kelseyann77 <79773318+kelseyann77@users.noreply.github.com>
2024-02-27 19:00:50 -0700

    modified setGame function
    adjusted details of gameboard and working on adding pieces to the board

--- connectfour/connect4.js
+++ connectfour/connect4.js
@@ -7,4 +7,61 @@ var board;
 
 var rows = 6;
 var columns = 7;
-var currentColumns = [];
+
+// When the page loads, we want the game to be setup
+window.onload = function() {
+    setGame();
+}
+
+// This function sets up the game
+function setGame() {
+    
+    // Create the array for the entire board
+    board = [];
+
+    // Create a for loop that will loop through all the tiles
+    // First create an array to account for each row
+    for ( let r = 0; r < rows; r++ ) {
+        let row = [];
+
+        // Traverse through each column
+        for ( let c = 0; c < columns; c++ ) {
+            row.push(' ');
+
+            // Create HTML to add <div id="0-0" class="tile"></div>
+            // inside the board
+            let tile = document.createElement( "div" );
+
+            // the ids will look like: 0-0, 0-1, 0-2, ...
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add( "tile" );
+
+            // Add Event Listener to add a new piece whenever users click on a column
+            // setPiece is a function that will be called whenever a column is clicked
+            tile.addEventListener("click", setPiece);
+
+            // within <div id ="board">, we want to add the newly created tiles
+            document.getElementById( "board" ).append( tile );
+        } // end for c
+
+        board.push( row );
+
+    } // end for r
+} // end function setGame
+
+function setPiece() {
+
+    // We will check if the game is over
+    // If the game is over, then nothing will happen
+    // No one will be able to set pieces (until the game is reset)
+    if (gameOver) {
+        return;
+    }
+
+    // If the game is not over, then we will obtain the coordinates of the tile that has been clicked
+    // Example: if 3-5 is clicked, coords will be set to [3,5]
+    //          then r = 3 and c = 5
+    let coords = this.id.split("-");
+    let r = parseInt(coords[0]);
+    let c = parseInt(coords[1]);
+}
